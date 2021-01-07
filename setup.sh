bc9ce78cc9700b5769fdbfdac57eb954ed204e0f
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-07 22:11:33 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ cd
 mkdir -p /media/hdd/epg
 wget -O /usr/script/xmltv.sh "https://github.com/ravstar/Enigma2/raw/main/xmltv.sh"
 sed -i 's,http://url:port/xmltv.php?username=uuuuuu&password=pppppp&next_days=7,/media/hdd/epg/iptvepg.xml.gz,g' /etc/epgimport/der
-sed -i 's/link/url:port/g;s/nnnnnn/uuuuuu/g;s/wwwwww/pppppp' /usr/script/xmltv.sh
+sed -i 's,link,url:port,g;s,nnnnnn,uuuuuu,g;s,wwwwww,pppppp,g' /usr/script/xmltv.sh
 sed -i 's/time/temp/g' /usr/script/xmltv.sh
 cd
 crontab -l | { cat; echo "00 06 * * * cd /usr/script/ && ./xmltv.sh "; } | crontab -
