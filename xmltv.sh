b01138c3582fb0106b276086305513b614357bc0
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-07 18:44:07 +0000

    Update xmltv.sh

--- xmltv.sh
+++ xmltv.sh
@@ -1,4 +1,4 @@
-wget -O /media/hdd/epg/iptvepg.xml "url"
-sed -i 's/+0000/-0100/g' /media/hdd/epg/iptvepg.xml
+wget -O /media/hdd/epg/iptvepg.xml "link"
+sed -i 's/+0000/time/g' /media/hdd/epg/iptvepg.xml
 gzip -f /media/hdd/epg/iptvepg.xml > /media/hdd/epg/iptvepg.xml.gz
 exit
