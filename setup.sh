727131cdab6e655dcf475f98e3dc5e31909b4d2b
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-07 19:29:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p /media/hdd/epg
 wget -O /usr/script/xmltv.sh "https://github.com/ravstar/Enigma2/raw/main/xmltv.sh"
-sed -i 's+url+/media/hdd/epg/iptvepg.xml.gz+g' /etc/epgimport/der
-sed -i 's+link+url+g' /usr/script/xmltv.sh
+sed -i 's,http://url:port/xmltv.php?username=uuuuuu&password=pppppp&next_days=7,/media/hdd/epg/iptvepg.xml.gz,g' /etc/epgimport/der
+sed -i 's,link,http://url:port/xmltv.php?username=uuuuuu&password=pppppp&next_days=7,g' /usr/script/xmltv.sh
 sed -i 's/time/temp/g' /usr/script/xmltv.sh
 crontab -l | { cat; echo "00 06 * * * cd /usr/script/ && ./xmltv.sh "; } | crontab -
 cd /usr/script/xmltv.sh
