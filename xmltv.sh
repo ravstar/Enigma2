d82aaf99fefa07992135787f7d0e1624dd9aad35
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-07 22:05:37 +0000

    Add files via upload

--- xmltv.sh
+++ xmltv.sh
@@ -1,4 +1,4 @@
-wget -O /media/hdd/epg/iptvepg.xml "link"
+wget -O /media/hdd/epg/iptvepg.xml "http://link/xmltv.php?username=nnnnnn&password=wwwwww&next_days=7"
 sed -i 's/+0000/time/g' /media/hdd/epg/iptvepg.xml
 gzip -f /media/hdd/epg/iptvepg.xml > /media/hdd/epg/iptvepg.xml.gz
 exit
