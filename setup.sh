ad28c425b354ba2c52aac19e63bbaeed6d61abfb
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-08 13:54:22 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ find . -type f -name "jmx.*.sources.xml" -exec cp {} /media/hdd/epg \;
 cd /media/hdd/epg/ && mv *.sources.xml new.EPG.sources.xml
 mv /media/hdd/epg/new.EPG.sources.xml /etc/epgimport
 sed -i 's,llllll/xmltv.php?username=uuuuuu&password=pppppp&next_days=7,/media/hdd/epg/iptvepg.xml.gz,g' /etc/epgimport/new.EPG.sources.xml
-sed -i 's/sourcecatname="/ sourcecatname="New-EPG_/g' /etc/epgimport/new.EPG.sources.xml
+sed -i 's/sourcecatname="/sourcecatname="New-EPG_/g' /etc/epgimport/new.EPG.sources.xml
 sed -i 's/<description>/<description>New-EPG_/g' /etc/epgimport/new.EPG.sources.xml
 wget -O /usr/script/xmltv.sh "https://github.com/ravstar/Enigma2/raw/main/xmltv.sh"
 sed -i 's,link,llllll,g;s,nnnnnn,uuuuuu,g;s,wwwwww,pppppp,g' /usr/script/xmltv.sh
