9cbbcaa4c5b203bd640ea4bed9010a9d2a056d8a
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-10 18:22:18 +0000

    Add files via upload

--- xmltv.sh
+++ xmltv.sh
@@ -1,4 +1,21 @@
-wget -O /media/hdd/epg/iptvepg.xml "link/xmltv.php?username=nnnnnn&password=wwwwww&next_days=7"
-sed -i '/+0000/ s//time/g' /media/hdd/epg/iptvepg.xml
-gzip -f /media/hdd/epg/iptvepg.xml > /media/hdd/epg/iptvepg.xml.gz
+#!/bin/sh
+#
+
+echo "Downloading EPG data..."
+
+wget -O dddddd "hhhhhh"
+
+clock=$(date '+%H:%M:%S:')
+echo $clock "CORRECTING TIME. PLEASE WAIT, IT WOULD TAKE BETWEEN 10-50 MINUTES SO BE PATIENT..."
+
+
+sed -i '/+0000/ s//tttttt/g' dddddd
+gzip -f dddddd > dddddd.gz
+
+sed -i "s|hhhhhh|dddddd.gz|g" ssssss
+
+clock1=$(date '+%H:%M:%S:')
+echo $clock1 "All done! go to epgimpoter, Look for option named 'Clearing current EPG before import' and turn it to yes and Import EPG manually by pressing yellow button"
+
+
 exit
