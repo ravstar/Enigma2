43c2d6404bc4960ce7bfb154d3fa021e9319de33
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-10 18:34:30 +0000

    Add files via upload

--- xmltv.sh
+++ xmltv.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 #
 
+xmltvfilename="dddddd"
+url="hhhhhh"
+time="tttttt"
+source="ssssss"
+
 echo "Downloading EPG data..."
 
-wget -O dddddd "hhhhhh"
+wget -O ${xmltvfilename} "${url}"
 
 clock=$(date '+%H:%M:%S:')
 echo $clock "CORRECTING TIME. PLEASE WAIT, IT WOULD TAKE BETWEEN 10-50 MINUTES SO BE PATIENT..."
 
+sed -i '/+0000/ s//${time}/g' ${xmltvfilename}
 
-sed -i '/+0000/ s//tttttt/g' dddddd
-gzip -f dddddd > dddddd.gz
+gzip -f  ${xmltvfilename} > ${xmltvfilename}.gz
 
-sed -i "s|hhhhhh|dddddd.gz|g" ssssss
+#replace source - double quotes required for sed to convert variables. 
+sed -i "s|$url|${xmltvfilename}.gz|g" $source
+
+
 
 clock1=$(date '+%H:%M:%S:')
 echo $clock1 "All done! go to epgimpoter, Look for option named 'Clearing current EPG before import' and turn it to yes and Import EPG manually by pressing yellow button"
