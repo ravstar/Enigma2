ad19cf3ff0259c9bc33b25ad40fc5053fcd083e6
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-12 19:57:40 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,18 +1,95 @@
-cd
-mkdir -p /media/hdd/epg
-cd /etc/epgimport/
-find . -type f -name "ffffff" -exec cp {} /media/hdd/epg \;
-cd /media/hdd/epg/ && mv ffffff new.EPG.sources.xml
-mv /media/hdd/epg/new.EPG.sources.xml /etc/epgimport
-sed -i 's,llllll/xmltv.php?username=uuuuuu&password=pppppp&next_days=7,/media/hdd/epg/iptvepg.xml.gz,g' /etc/epgimport/new.EPG.sources.xml
-sed -i 's/sourcecatname="/sourcecatname="New-EPG_/g' /etc/epgimport/new.EPG.sources.xml
-sed -i 's/<description>/<description>New-EPG_/g' /etc/epgimport/new.EPG.sources.xml
+#!/bin/sh
+#
+echo "uninstalling previous version of this script"
+
+# Remove old script data
+rm /etc/epgimport/new.EPG.sources.xml
+rm /etc/hdd/epg/iptvepg.xml.gz
+# Removing Cron
+crontab -l | grep -v "\00 \06 \* \* \* cd /usr/script/ && ./xmltv.sh" | crontab -
+echo "starting new installation"
+
+workdir="/media/hdd/epg"
+epgimport="/etc/epgimport"
+echo
+echo "This script will fix EPG offset issue for IPTV i.e. if your epg is ahead or behind and showing wrong program information. 
+You can adjust the offset time between any range from 1 minute to up to 23 hours."
+echo
+sleep 6
+
+count=$(cd ${epgimport}/ && ls -d *jmx.*.sources.xml* | wc -l)
+if [ $count -eq "1" ]; then
+   filename=$(find ${epgimport}/ -name "jmx.*.sources.xml" -exec basename {} \;)
+   echo "One jedimaker playlist found: $filename, proceeding installation..."
+   sleep 2
+else
+echo "Enter full file name from below list starting with jmx.XXXX.sources.xml, For example: You want to fix EPG offset for provider's playlist named IPTV in jedimakerxtreme then you should look for file named jmx.IPTV.sources.xml"
+echo
+find $epgimport/ -name "jmx.*.sources.xml" -exec basename {} \;
+echo
+read -p "Enter file name: " filename
+fi
+filename1=$(cd ${epgimport}/ && ls ${filename})
+if [ "$filename1" == "$filename" ]; then 
+echo
+else
+echo "Wrong input installation aborted. Try to run the script again by typing ./setup.sh below and hit enter"
+echo
+exit
+fi
+echo
+echo "Enter the time you would like to offset including +/-HHMM
+Example:
+        your epg is half an hour ahead then put -0030
+        your epg is an hour ahead then put -0100 
+	your epg is two hours ahead then put -0200 
+	your epg is half an hour behind then put +0030 
+	your epg is an hour behind then put +0100 
+	your epg is two hours behind then put +0200, and so on.."
+echo
+read -p "Enter time: " time
+echo
+echo $filename
+echo
+echo $time
+if [ ! -d "${workdir}" ]; then
+  mkdir -p "${workdir}" 
+fi
+source="${epgimport}/$filename"
+# get url from source file
+url=$(grep -o 'http.*$' ${source} | cut -f 1 -d ']')
+echo $url
+#get name from source file, remove illegal file characters with underscore and make lower case
+name=$(grep -o 'catname=.*$' ${source} | cut -c10- | cut -f 1 -d '"' | \
+sed -e 's|<|_|g; s|>|_|g; s|:|_|g; s|"|_|g; s|/|_|g; s|\\|_|g; s/|/_/g; s|?|_|g; s|*|_|g; s| |_|g')
+echo $name
+
+#download xmltv file (xmltv.sh) to filename $xmltvfilename - replace old time with new time
+xmltvfilename="${workdir}/${name}.xml"
+echo $xmltvfilename
 wget -O /usr/script/xmltv.sh "https://github.com/ravstar/Enigma2/raw/main/xmltv.sh"
-sed -i 's,link,llllll,g;s,nnnnnn,uuuuuu,g;s,wwwwww,pppppp,g' /usr/script/xmltv.sh
-sed -i 's/time/temp/g' /usr/script/xmltv.sh
-cd
-crontab -l | { cat; echo "00 06 * * * cd /usr/script/ && ./xmltv.sh "; } | crontab -
-cd /usr/script/
-chmod 755 xmltv.sh
-./xmltv.sh
+sed -i "s|\\&|$url|" "/usr/script/xmltv.sh"
+sed -i "s|dddddd|$xmltvfilename|; s|tttttt|$time|; s|ssssss|$source|" "/usr/script/xmltv.sh"
+cd /usr/script/ && chmod 755 xmltv.sh
+wget -O /usr/script/uninstall1.sh "https://github.com/ravstar/Enigma2/raw/main/uninstall1.sh"
+sed -i "s|dddddd|$xmltvfilename|; s|tttttt|$time|; s|ssssss|$source|" "/usr/script/uninstall1.sh"
+cd /usr/script/ && chmod 755 uninstall1.sh
+
+crontab -l | { cat; echo "59 05 * * * cd /usr/script/ && ./xmltv.sh "; } | crontab -
+
+HHMM=$(wget "${url}" -O -| grep -o 'start=".*$' | cut -c 23-27 | cut -f 1 -d '"' | head -1)
+echo
+echo "xml HHMM is $HHMM"
+echo
+if [ $HHMM -eq "+0000" ]; then
+   cd /usr/script/ &&./xmltv.sh
+else
+echo "ERROR: Script has been installed though it won't work until you follow one more step manually. Please make a note of following text"
+echo
+echo "My xml HHMM is: $HHMM and I want to adjust it by: $time"
+echo
+echo "Please follow the additional step mentioned in original post or reach out to 'ravstar'"
+echo
+fi
 rm /tmp/setup.sh
+exit
