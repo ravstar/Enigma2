22c174c4d9e6f9693cf9e6f77ec96c6f2daada9d
ravstar <77118581+ravstar@users.noreply.github.com>
2021-01-12 19:35:13 +0000

    Add files via upload

--- uninstall1.sh
+++ uninstall1.sh
@@ -7,23 +7,19 @@ url=$(grep -o 'url=".*$' /usr/script/xmltv.sh | cut -c6- | cut -f 1 -d '"')
 echo $url
 source="ssssss"
 
-# Remove old script data
-rm /etc/epgimport/new.EPG.sources.xml
-rm /etc/hdd/epg/iptvepg.xml.gz
-
 # Removing epg data
 rm ${xmltvfilename}.gz
 
-sed -i "s|$xmltvfilename.gz|&|" $source
+sed -i "s|$xmltvfilename.gz|\\&|" $source
 sed -i "s|\\&|$url|" $source
 
 # Removing Cron
-crontab -l | grep -v "\00 \06 \* \* \* cd /usr/script/ && ./xmltv.sh" | crontab -
+crontab -l | grep -v "\59 \05 \* \* \* cd /usr/script/ && ./xmltv.sh" | crontab -
 
 # Removing script
 rm /usr/script/xmltv.sh
 
 echo "script and crontab successfully uninstalled and source file restored"
 
-rm /usr/script/uninstall.sh
+rm /usr/script/uninstall1.sh
 exit
